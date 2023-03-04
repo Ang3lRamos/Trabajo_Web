8b51fb3abd1b431f0e0c31d7d71dde83581da72b
Ang3lRamos <angelf.ramosc@unac.edu.co>
2023-03-04 10:46:28 -0500

    Avance en web

---
+++ ProyectoWeb/acccion.js
@@ -0,0 +1,39 @@
+function login(e) {
+    //Para ejecucion
+    e.preventDefault();
+    console.log("kmi boba")
+
+    //Para datos
+    let inputUser = document.getElementById("usuario").value
+    let inputPassword = document.getElementById("contraseña").value
+
+    console.log(inputUser)
+    console.log(inputPassword)
+
+    fetch("http://89.116.25.43:3000/api/login", {
+            method:"POST",
+            body: JSON.stringify({
+                usuario: inputUser,
+                password: inputPassword
+            }),
+            headers:{
+                "Content-Type": "application/json"
+            }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        sessionStorage.setItem("token",data["jwt"])
+        sessionStorage.setItem("user",JSON.stringify(data.user))
+    })
+    .then(redirect => location.href = "dashboard.html")
+}
+
+function verDatos() {
+    fetch("http://89.116.25.43:3000/api/usuarios/listar",{
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+verDatos()
