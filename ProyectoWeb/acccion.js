9056abc4b7a4c04ddb0619e4f870597db761eb94
Ang3lRamos <angelf.ramosc@unac.edu.co>
2023-03-11 11:54:13 -0500

    Nuevos metodos y boton agregar

--- ProyectoWeb/acccion.js
+++ ProyectoWeb/acccion.js
@@ -1,3 +1,26 @@
+function logout(){
+    sessionStorage.clear();
+    location.href("login.html");
+}
+
+function auth(){
+    const token = sessionStorage.getItem("token");
+    if(token | token === undefined){
+        location.href("login.html");
+    }
+}
+function authLogin(){
+    const token = sessionStorage.getItem("token");
+    if(token){
+        location.href("dashboard.html");
+    }
+}
+function defName(){
+    const dataUser = sessionStorage.getItem("user");
+    const user = JSON.parse(dataUser);
+    let userName = `<h5>`+user.usuario.toUpperCase()+`</h5>`;
+    $("#content-username").append(userName);
+}
 function login(e) {
     //Para ejecucion
     e.preventDefault();
@@ -26,6 +49,7 @@ function login(e) {
         sessionStorage.setItem("token",data["jwt"])
         sessionStorage.setItem("user",JSON.stringify(data.user))
 
+        // data.status === 200 ? location.href = "dashboard.html" : data.status === 403 ? alert("No tiene permisos para ingresar") : alert("Ha ocurrido un error inesperado")
         if(data.status === 200){
             location.href = "dashboard.html"
         }else if(data.status === 403){
@@ -36,7 +60,36 @@ function login(e) {
     })
 }
 
+function registrar(e) {
+    //Para ejecucion
+    e.preventDefault();
 
+    //Para datos
+    let inputNombre = document.getElementById("nombres").value;
+    let inputApellidos = document.getElementById("apellidos").value;
+    let inputTelefono = document.getElementById("telefono").value;
+    let inputDireccion = document.getElementById("direccion").value;
+    let inputEmail = document.getElementById("email").value;
+    let inputUsername = document.getElementById("usuario").value;
+    let inputPassword = document.getElementById("pass").value;
+
+    fetch("",{
+        method: "POST",
+        body: JSON.stringify({
+            nombres: inputNombre,
+            apellidos: inputApellidos,
+            telefono: inputTelefono,
+            email: inputEmail,
+            direccion: inputDireccion,
+            usuario: inputUsername,
+            password: inputPassword
+        }),
+        headers: {
+            "Content-type": "application/json"
+        },
+    })
+    .then(response => location.href = "dashboard.html")
+}
 
 // function verDatos() {
 //     fetch("http://89.116.25.43:3000/api/usuarios/listar",{
